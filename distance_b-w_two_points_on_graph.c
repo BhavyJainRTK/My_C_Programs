9ba3d7476e0b8d69e56e4b0f3e81e1ab92d1a78f
BhavyJain <bhavy2142006@gmail/com>
2025-07-02 19:19:02 +0530

    add new program

---
+++ distance_b-w_two_points_on_graph.c
@@ -0,0 +1,19 @@
+// find the distance between two points on the graps :
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float x1,y1,x2,y2,distance;
+    
+    printf("enter the frist co-ordinat (x1,y1) : ");
+    scanf("%f %f",&x1,&y1);
+
+    printf("enter the second co-ordinat (x2,y2) : ");
+    scanf("%f %f",&x2,&y2);
+
+    distance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+
+    printf("the distance between the points is = %f",distance);
+}
