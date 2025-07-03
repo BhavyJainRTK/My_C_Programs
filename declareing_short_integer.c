e5c9e849fe29a045cf0a429f1e37ae13db247126
BhavyJain <bhavy2142006@gmail/com>
2025-07-03 19:54:58 +0530

    add new program

---
+++ declareing_short_integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    short int x;
+    int y;
+    long int z;
+
+    printf("short int contain value from -32,768 to 32,767\n");
+    printf("int contain value from -2,147,483,648 to 2,147,483,647\n");
+    printf("lont it contain value Up to +-9 quintillion");
+}
