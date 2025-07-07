201b5ecaa7b8d5b3a697a9080cae7592f7e008bc
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-07 19:19:02 +0530

    add new program

---
+++ sin_cos_values.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float n;
+    int angle[50];
+    int num,i;
+
+    printf("how many angle will you enter");
+    scanf("%d",&num);
+    
+    for (i = 0; i < num; i++)
+    {
+        printf("enter the angle : ");
+        scanf("%d",&angle[i]);
+    
+    }
+    
+    printf("________________________________");
+    printf("|  ANGLES  |  SIN X  |  COS X  |\n");
+
+    for ( i = 0; i < num; i++)
+    {
+        n = angle[i] * 0.017453;
+    printf("|    %d    |   %.2f  |  %.2f   |\n",angle[i], sin(n),cos(n));
+    }
+
+    printf("|__________|_________|_________|");
+    
+    
+    
+    
+    
+}                
