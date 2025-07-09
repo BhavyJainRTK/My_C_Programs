d33c2f926c5d1ab9fc59d90fca4aa5a461deb207
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-09 23:07:29 +0530

    add new program

---
+++ multiplication_two_digit_numbers_by_proper_multiplication.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1,n2,x1,x2;
+
+    printf("enter two two-digits numbers only > ");
+    scanf("%d %d",&n1,&n2);
+
+    printf("%20d\n             X%6d\n",n1,n2);
+    printf("             ---------\n");
+    
+    x1 = n2 % 10;
+    x2 = n2 / 10;
+
+    printf("%d X %d  is %9d\n",x1,n1,x1*n1);
+    printf("%d X %d  is %8dX\n",x2,n1,x2*n1);
+    printf("             ---------\n");
+    printf("  ADD  them %8d \n",(x1*n1)+((x2*n1)*10));
+    printf("             ---------\n");
+}
