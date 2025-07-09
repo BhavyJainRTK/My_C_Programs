7b7c4690a08043eadc65afdd9bb46a2fb0a0806d
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-09 18:48:59 +0530

    edit program

--- convert_float_num_to_integers.c
+++ convert_float_num_to_integers.c
@@ -2,26 +2,20 @@
 
 int main()
 {
-    float num1,num2,num3,num4;
-    int i,j;
+    float num[4];
+    int i;
 
     printf("YOU HAVE TO ENTER [4] NUMBERS\n");
 
-    printf("enter 1st number > ");
-    scanf("%f",&num1);
-
-    printf("enter 2nd number > ");
-    scanf("%f",&num2);
-
-    printf("enter 3rd number > ");
-    scanf("%f",&num3);
-
-    printf("enter 4th number > ");
-    scanf("%f",&num4);
-
-    printf("1. %.2f > %.0f\n",num1,num1);
-    printf("2. %.2f > %.0f\n",num2,num2);
-    printf("3. %.2f > %.0f\n",num3,num3);
-    printf("4. %.2f > %.0f\n",num4,num4);
+    for ( i = 0; i < 4; i++)
+    {
+        printf("enter [%d] number > ",i+1);
+        scanf("%f",&num[i]);
+    }
     
+    for ( i = 0; i < 4; i++)
+    {
+        printf("1. %.2f > %.0f\n",num[i],num[i]);
+    }
+
 }
