167a63eae08d0846fdd049da6837eddf1082c335
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-12 00:59:26 +0530

    add new rpogram

---
+++ reversing_the_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,x,i=10000;
+    int fn = 0;
+
+    printf("ENTER A FIVE DIGIT NUMBER > ");
+    scanf("%d",&num);
+
+    while (i>=1)
+    {
+        x = (num % 10)*i;
+        fn = fn + x;
+        num = num / 10;
+        i = i / 10;
+    }
+    printf("NUMBER > %d",fn);
+    
+}
