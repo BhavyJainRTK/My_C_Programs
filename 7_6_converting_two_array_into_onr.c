d24a0e8d4521abd05a4c9a0c365508354146232b
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-15 00:35:21 +0530

    edit program

--- 7_6_converting_two_array_into_onr.c
+++ 7_6_converting_two_array_into_onr.c
@@ -2,12 +2,17 @@
 
 int main()
 {
+    // DECLEARING VARIABLE AND ARRAY.
+
     int x, y;
     int a[10];
     int b[10];
     int c[20];
 
     int i, j, k;
+
+    // SAVI NUMBER IN ARRAY A AND B.
+    // USING LOOP.
 
     printf("HOW MANY NUMBER YOU WANT TO ENTER IN ARRAY A > ");
     scanf("%d", &x);
@@ -30,6 +35,10 @@ int main()
     i = 0;
     j = 0;
     k = 0;
+
+    // MERGEING ARRAY A AND B IN ARRAY C.
+    // IN ASCENDING ORDER.
+
     while (i < x && j < y)
     {
         if (a[i] < b[j])
@@ -37,6 +46,8 @@ int main()
         else
             c[k++] = b[j++];
     }
+
+    // SAVING REST OF NUMBER.
 
     while (i < x)
     {
@@ -46,6 +57,8 @@ int main()
     {
         c[k++] = b[j++];
     }
+
+    // PRINTING ARRAY C.
 
     for (i = 0; i < k; i++)
     {
