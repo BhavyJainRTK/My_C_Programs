328683032a34112050a67bcb2735aae21cad35e2
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-14 23:40:02 +0530

    adit program

--- damped_natural_frequency.c
+++ damped_natural_frequency.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    float l,c,r,a;
+    float l, c, r, a;
 
     printf("enter the inductance in the circuit : ");
-    scanf("%f",&l);
+    scanf("%f", &l);
 
     printf("enter the resistance in the circuit : ");
-    scanf("%f",&r);
+    scanf("%f", &r);
 
     printf("enter the capacitance in the circuit : ");
-    scanf("%f",&c);
+    scanf("%f", &c);
 
-    a = (1/(l*c)) - ((r*r)/(4*(c*c)));
+    a = (1 / (l * c)) - ((r * r) / (4 * (c * c)));
 
-    printf("FREQUENCY = %f",sqrt(a));
+    printf("FREQUENCY = %f", sqrt(a));
 }
