6de6aafd1145fb41e7379ef87aab5eee15d59db6
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-14 23:39:48 +0530

    adit program

--- convert_float_num_to_integers.c
+++ convert_float_num_to_integers.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
@@ -7,15 +7,14 @@ int main()
 
     printf("YOU HAVE TO ENTER [4] NUMBERS\n");
 
-    for ( i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
     {
-        printf("enter [%d] number > ",i+1);
-        scanf("%f",&num[i]);
-    }
-    
-    for ( i = 0; i < 4; i++)
-    {
-        printf("1. %.2f > %.0f\n",num[i],num[i]);
+        printf("enter [%d] number > ", i + 1);
+        scanf("%f", &num[i]);
     }
 
+    for (i = 0; i < 4; i++)
+    {
+        printf("1. %.2f > %.0f\n", num[i], num[i]);
+    }
 }
