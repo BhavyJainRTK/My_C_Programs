78ba605ba23f2522a145be7623ba4ef8f7497bab
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-15 19:06:53 +0530

    EDIT PROGRAM

--- 7_13_transpose_of_a_matrix.c
+++ 7_13_transpose_of_a_matrix.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
+    // DECLEARING VARIABLES AND MATRIX.
+
     int matrix[10][10];
     int transpose[10][10];
-    int m,n;
+    int m, n;
 
     printf("ENTER THE DIMENTIONS OF THE MATRIX M & N > ");
-    scanf("%d %d",&m,&n);
-    
+    scanf("%d %d", &m, &n);
+
+    // TARING INPUT FROM USER.
+
     printf("ENTER THE MATRIX\n");
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
             printf("> ");
-            scanf("%d",&matrix[i][j]);
+            scanf("%d", &matrix[i][j]);
         }
     }
+
+    // PRINTING THE MATRIX.
 
     printf("ORIGINAL MATRIX\n");
 
@@ -25,12 +31,14 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%d  ",matrix[i][j]);
+            printf("%d  ", matrix[i][j]);
         }
         printf("\n");
     }
-    
+
     printf("\n");
+
+    // CONVERTING ORIGINAL MATRIX TO TRANSPOSE.
 
     for (int j = 0; j < n; j++)
     {
@@ -39,14 +47,16 @@ int main()
             transpose[j][i] = matrix[i][j];
         }
     }
-     
+
+    // PRINTING TRANSPOSED MATRIX.
+
     printf("TRANSPOSED MATRIX\n");
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
-            printf("%d  ",transpose[i][j]);
+            printf("%d  ", transpose[i][j]);
         }
         printf("\n");
     }
