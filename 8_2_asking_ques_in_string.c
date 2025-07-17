08a83dfd548f86139e697cb23392116f2525489b
BhavyJainRTK <bhavy2142006@gmail.com>
2025-07-17 18:37:16 +0530

    add new program

--- 8_2_asking_ques_in_string.c
+++ 8_2_asking_ques_in_string.c
@@ -1,26 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
+    // DECLEARING THE STRING.
+
     char correct_answer[6] = "earth";
     char user_answer[10];
 
+    // TAKING INPUT FROM USER.
 
+    printf("you have only three attempts\n");
     printf("What is name of our planet ?");
-    for ( int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         printf("\nANSWER > ");
-        scanf("%s",user_answer);
+        scanf("%s", user_answer);
 
-        
+        // CONVERTING THE USER STRING TO LOWER CASE.
+
         for (int i = 0; user_answer[i] != '\0'; i++)
         {
-            user_answer[i]= tolower(user_answer[i]);
+            user_answer[i] = tolower(user_answer[i]);
         }
-        
-        if (strcmp(correct_answer,user_answer) == 0)
+
+        // COMPAIRING THE USER STRING TO MAIN STRING.
+
+        if (strcmp(correct_answer, user_answer) == 0)
         {
             goto correct;
         }
@@ -30,10 +37,14 @@ int main()
         }
     }
 
+    // IF ANSWER WRONG.
+
     printf("\nThe correct answer is [Earth]");
     return 0;
 
-    correct :
+    // IF ANSWER CORRECT.
+
+correct:
     printf("\nGOOD");
     return 1;
 }
