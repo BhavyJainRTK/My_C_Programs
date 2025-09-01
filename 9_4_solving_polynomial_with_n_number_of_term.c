bd55d54f9c09a7e21b21e85872a0015f92c21538
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-01 22:24:16 +0530

    edit progrogram

--- 9_4_solving_polynomial_with_n_number_of_term.c
+++ 9_4_solving_polynomial_with_n_number_of_term.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 
-int add(int a[0],int n,int x);
+int add(int a[0], int n, int x); // declearing the function
 
-int sol,i,solution=1;
+int sol, i, solution = 1;
 int main()
 {
-    int n_terms,x;
+    int n_terms, x;
     int value_a[10];
 
     printf("Enter the number of terms > ");
-    scanf("%d",&n_terms);
+    scanf("%d", &n_terms); // taking number of terms from user
 
     printf("Enter the value of x > ");
-    scanf("%d",&x);
+    scanf("%d", &x);
 
-    for ( i = 1; i <= n_terms; i++)
+    for (i = 1; i <= n_terms; i++)
     {
-        printf("Enter a%d = ",i);
-        scanf("%d",&value_a[i-1]);
+        printf("Enter a%d = ", i);
+        scanf("%d", &value_a[i - 1]); // saving the different values using loop
     }
 
-    int solution = add(value_a,n_terms,x);
+    int solution = add(value_a, n_terms, x); // calling the function
 
-    printf("Solution > %d",solution);
-
+    printf("Solution > %d", solution);
 }
 
-int add(int a[0],int n,int x)
+int add(int a[0], int n, int x)
 {
     int result = a[0];
 
     for (int i = 0; i < n; i++)
     {
-        result = result * (x + a[i]);
+        result = result * (x + a[i]); // solving each term and saving solution
     }
+
     return result;
-    
 }
