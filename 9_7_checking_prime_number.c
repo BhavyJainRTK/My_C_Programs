48a6f62d2e3bc8e64012614522ff3d6c7f4e23cd
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-01 22:45:07 +0530

    add new program

---
+++ 9_7_checking_prime_number.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int prime(int n);
+
+int x = 0;
+int main()
+{
+    int number,result;
+    printf("Enter a number > ");
+    scanf("%d",&number);
+
+    result = prime(number);
+    printf("1 > yes | 0 > no\n");
+    printf("ans > %d",result);
+}
+
+int prime(int n)
+{
+    for (int i = n; i > 0 ; i--)
+    {
+        if ((n%i) == 0)
+        {
+            x = x + 1;
+        } 
+    }
+
+    if (x == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+    
+}
