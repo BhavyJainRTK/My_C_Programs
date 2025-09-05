2685923512e07430d717333c622d4841e604aec3
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-05 11:34:49 +0530

    edit program

--- number_of_digit_at_every_positon_of_matrix.c
+++ number_of_digit_at_every_positon_of_matrix.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+int calculate(int a[3][3],int b[3][3]);
+
+int  count = 0;
 int main()
 {
-    int matrix[3][3], e_matrix[3][3], count = 0;
+    int matrix[3][3], e_matrix[3][3];
 
     printf("Enter the elements of matrix > \n");
 
@@ -15,33 +18,38 @@ int main()
         }
     }
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            count = 0;
-
-            if (matrix[i][j] == 0)
-            {
-                count = 1;
-            }
-            else
-            {
-                while (matrix[i][j] > 0 || matrix[i][j] < 0)
-                {
-                    matrix[i][j] = matrix[i][j] / 10;
-                    count++;
-                }
-            }
-            e_matrix[i][j] = count;
-        }
-    }
+    calculate(matrix,e_matrix);
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
             printf("\na%d%d have %d digits.", i + 1, j + 1, e_matrix[i][j]);
+        }
+    }
+}
+
+int calculate(int a[3][3],int b[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            count = 0;
+
+            if (a[i][j] == 0)
+            {
+                count = 1;
+            }
+            else
+            {
+                while (a[i][j] > 0 || a[i][j] < 0)
+                {
+                    a[i][j] = a[i][j] / 10;
+                    count++;
+                }
+            }
+            b[i][j] = count;
         }
     }
 }
