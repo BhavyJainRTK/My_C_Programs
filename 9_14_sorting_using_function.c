565e169d910e544f9e66f98c65efd0c8990388a7
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-06 20:39:42 +0530

    add new program

---
+++ 9_14_sorting_using_function.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int sorting(int a[10]);
+
+int n;
+int main()
+{
+    int array1[10],array2[10];
+
+    printf("how many element yuo want to enter is the arrays > ");
+    scanf("%d",&n);
+
+    printf("enter elements for first array > ");
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&array1[i]);
+    }
+    
+    printf("enter elements for second array > ");
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&array2[i]);
+    }
+
+    sorting(array1);
+    sorting(array2);
+    
+    printf("sorted array 1 > ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d, ",array1[i]);
+    }
+    
+    printf("\n");
+
+    printf("sorted array 2 > ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d, ",array2[i]);
+    }
+    
+}
+
+int sorting(int a[10])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp; 
+            }
+        }
+    }
+
+}
