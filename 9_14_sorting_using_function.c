a0e4c8ccbe04529f8274ff4adea670ac417bf65c
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-06 20:57:23 +0530

    edit program

--- 9_14_sorting_using_function.c
+++ 9_14_sorting_using_function.c
@@ -1,49 +1,25 @@
 #include<stdio.h>
 
-int sorting(int a[10]);
+int sorting(int a[10],int n);
+int creat_array(int a[10],int b[10],int n);
+int print_array(int a[10],int b[10],int n);
 
-int n;
 int main()
 {
-    int array1[10],array2[10];
+    int array1[10],array2[10],n;
 
     printf("how many element yuo want to enter is the arrays > ");
     scanf("%d",&n);  // number of element in array
 
-    printf("enter elements for first array > ");
+    creat_array(array1,array2,n);
+    sorting(array1,n);  // calling function
+    sorting(array2,n);  // calling function
 
-    for (int i = 0; i < n; i++)  // saving array 1 
-    {
-        scanf("%d",&array1[i]);
-    }
-    
-    printf("enter elements for second array > ");
-
-    for (int i = 0; i < n; i++)  // saving array 2
-    {
-        scanf("%d",&array2[i]);
-    }
-
-    sorting(array1);  // calling function
-    sorting(array2);  // calling function
-    
-    printf("sorted array 1 > ");  // printing sorted array 1
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d, ",array1[i]);
-    }
-    
-    printf("\n");
-
-    printf("sorted array 2 > ");  // printing sorted array 2
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d, ",array2[i]);
-    }
+    print_array(array1,array2,n);
     
 }
 
-int sorting(int a[10])  // sorting
+int sorting(int a[10],int n)  // sorting
 {
     for (int i = 0; i < n; i++)
     {
@@ -58,4 +34,41 @@ int sorting(int a[10])  // sorting
         }
     }
 
+}
+
+int creat_array(int a[10],int b[10],int n)
+{
+
+    printf("enter elements for first array > ");
+
+    for (int i = 0; i < n; i++)  // saving array 1 
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    printf("enter elements for second array > ");
+
+    for (int i = 0; i < n; i++)  // saving array 2
+    {
+        scanf("%d",&b[i]);
+    }
+
+    return n;
+}
+
+int print_array(int a[10],int b[10],int n)
+{
+    printf("sorted array 1 > ");  // printing sorted array 1
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d, ",a[i]);
+    }
+    
+    printf("\n");
+
+    printf("sorted array 2 > ");  // printing sorted array 2
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d, ",b[i]);
+    }
 }
