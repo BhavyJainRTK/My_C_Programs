3f67fba3a42be3fe0e8e9815574c4e58201f068a
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-20 21:15:03 +0530

    edit program

--- 10_5_incrementing_date.c
+++ 10_5_incrementing_date.c
@@ -15,18 +15,30 @@ struct date
 
 int month_name_to_number(char month[])
 {
-    if (strcmp(month, "january") == 0) return 1;
-    if (strcmp(month, "february") == 0) return 2;
-    if (strcmp(month, "march") == 0) return 3;
-    if (strcmp(month, "april") == 0) return 4;
-    if (strcmp(month, "may") == 0) return 5;
-    if (strcmp(month, "june") == 0) return 6;
-    if (strcmp(month, "july") == 0) return 7;
-    if (strcmp(month, "august") == 0) return 8;
-    if (strcmp(month, "september") == 0) return 9;
-    if (strcmp(month, "october") == 0) return 10;
-    if (strcmp(month, "november") == 0) return 11;
-    if (strcmp(month, "december") == 0) return 12;
+    if (strcmp(month, "january") == 0)
+        return 1;
+    if (strcmp(month, "february") == 0)
+        return 2;
+    if (strcmp(month, "march") == 0)
+        return 3;
+    if (strcmp(month, "april") == 0)
+        return 4;
+    if (strcmp(month, "may") == 0)
+        return 5;
+    if (strcmp(month, "june") == 0)
+        return 6;
+    if (strcmp(month, "july") == 0)
+        return 7;
+    if (strcmp(month, "august") == 0)
+        return 8;
+    if (strcmp(month, "september") == 0)
+        return 9;
+    if (strcmp(month, "october") == 0)
+        return 10;
+    if (strcmp(month, "november") == 0)
+        return 11;
+    if (strcmp(month, "december") == 0)
+        return 12;
     return 0;
 }
 
@@ -119,7 +131,8 @@ int main()
         display(first);
         next_date(&first);
 
-        for (int i = 0; i < 300000000; i++); 
+        for (int i = 0; i < 300000000; i++)
+            ;
     }
 
     return 0;
