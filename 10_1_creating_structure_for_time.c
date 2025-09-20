ce4ddd7ec59febc46ada1b3c6b9856df7a99b6f8
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-20 18:28:24 +0530

    add new program

---
+++ 10_1_creating_structure_for_time.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct time_struct   // creatinf a structure
+{
+    // variables for time 
+
+    int hour;
+    int min;
+    int sec;
+};
+
+int main()
+{
+    struct time_struct first;
+
+    printf("enter the details > ");
+
+    // taking input from user 
+
+    scanf("%d %d %d",
+          &first.hour,
+          &first.min,
+          &first.sec);
+
+    // print the result
+    
+    printf("\n%d:%d:%d",
+           first.hour,
+           first.min,
+           first.sec);
+
+    return 0;
+}
