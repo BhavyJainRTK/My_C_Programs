19029d21e92fc3cf527b1c05069c84a0921da670
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-21 13:11:23 +0530

    add new program

---
+++ 10_11_adding_two_lengths.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include<math.h>
+
+// declearing datatype
+
+struct metric
+{
+    float meter;
+    float centimeter;
+};
+
+struct british
+{
+    float foot;
+    float inch;
+};
+
+// input from user
+
+void input_m(struct metric *user)
+{
+    printf("Meters > ");
+    scanf("%f", &user->meter);
+
+    printf("Centimeter > ");
+    scanf("%f", &user->centimeter);
+}
+
+void input_b(struct british *user)
+{
+    printf("foot > ");
+    scanf("%f", &user->foot);
+
+    printf("inch > ");
+    scanf("%f", &user->inch);
+}
+
+// conversion into inch
+
+void convert_to_inch(struct metric *user, struct british *xuser)
+{
+    xuser->inch = xuser->inch + (user->centimeter / 2.54);
+    xuser->inch = xuser->inch + (user->meter * 39.3701);
+    xuser->inch = xuser->inch + (xuser->foot * 12.0);
+}
+
+// converting inch to foot
+
+void convert_to_foot(struct british xuser)
+{
+    int foot = xuser.inch / 12;
+    float inch = fmod(xuser.inch , 12);
+
+    printf("\n %d feet and %f inch.",foot,inch);
+}
+
+int main()
+{
+    struct metric user1;
+    struct british user2;
+
+    input_m(&user1);
+    input_b(&user2);
+
+    convert_to_inch(&user1,&user2);
+    convert_to_foot(user2);
+
+}
