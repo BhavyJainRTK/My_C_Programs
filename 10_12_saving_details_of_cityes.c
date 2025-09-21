a39f5a7e48984d770e084e8f5a3c314ab6d4f97c
BhavyJainRTK <bhavy2142006@gmail.com>
2025-09-21 18:28:57 +0530

    add new program

---
+++ 10_12_saving_details_of_cityes.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <string.h>
+
+struct census
+{
+    char city[5][20];
+    long int population[5];
+    int literacy_rate[5];
+
+    char temp_city[20];
+    long int temp_pop;
+    int temp_lit;
+};
+
+void input(struct census *first)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter city name > ");
+        scanf("%s", first->city[i]);
+        printf("Enter population > ");
+        scanf("%ld", &first->population[i]);
+        printf("Enter literacy rate in percentage > ");
+        scanf("%d", &first->literacy_rate[i]);
+    }
+}
+
+void name_ac(struct census *first)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = i+1; j < 5; j++)
+        {
+            if (strcmp(first->city[i], first->city[j]) > 0)
+            {
+                strcpy(first->temp_city, first->city[i]);
+                first->temp_pop = first->population[i];
+                first->temp_lit = first->literacy_rate[i];
+
+                strcpy(first->city[i], first->city[j]);
+                first->population[i] = first->population[j];
+                first->literacy_rate[i] = first->literacy_rate[j];
+
+                strcpy(first->city[j], first->temp_city);
+                first->population[j] = first->temp_pop;
+                first->literacy_rate[j] = first->temp_lit;
+            }
+        }
+    }
+}
+
+void population_sort(struct census *first)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = i+1; j < 5; j++)
+        {
+            if (first->population[i] > first->population[j])
+            {
+                strcpy(first->temp_city, first->city[i]);
+                first->temp_pop = first->population[i];
+                first->temp_lit = first->literacy_rate[i];
+
+                strcpy(first->city[i], first->city[j]);
+                first->population[i] = first->population[j];
+                first->literacy_rate[i] = first->literacy_rate[j];
+
+                strcpy(first->city[j], first->temp_city);
+                first->population[j] = first->temp_pop;
+                first->literacy_rate[j] = first->temp_lit;
+            }
+        }
+    }    
+}
+
+void literacy_rate_sort(struct census *first)
+{
+        for (int i = 0; i < 4; i++)
+    {
+        for (int j = i+1; j < 5; j++)
+        {
+            if (first->literacy_rate[i] > first->literacy_rate[j])
+            {
+                strcpy(first->temp_city, first->city[i]);
+                first->temp_pop = first->population[i];
+                first->temp_lit = first->literacy_rate[i];
+
+                strcpy(first->city[i], first->city[j]);
+                first->population[i] = first->population[j];
+                first->literacy_rate[i] = first->literacy_rate[j];
+
+                strcpy(first->city[j], first->temp_city);
+                first->population[j] = first->temp_pop;
+                first->literacy_rate[j] = first->temp_lit;
+            }
+        }
+    } 
+}
+
+void display(struct census first)
+{
+    printf("\n%-20s %-15s %-15s", "City", "Population", "Literacy Rate");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\n%-20s  %-15ld  %-15d",first.city[i],first.population[i],first.literacy_rate[i]);
+    }
+    
+}
+
+int main()
+{
+    struct census first;
+
+    input(&first);
+
+    printf("\ncities sorted alphabetically by name:\n");
+    name_ac(&first);
+    display(first);
+
+    printf("\ncities sorted in ascending order by population:\n");
+    population_sort(&first);
+    display(first);
+
+    printf("\ncities sorted in ascending order by literacy rate:\n");
+    literacy_rate_sort(&first);
+    display(first);
+}
